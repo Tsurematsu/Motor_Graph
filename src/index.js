143f1652a405542308741708b492bc8af020281e
Tsurematsu <daniel120802@gmail.com>
2024-01-19 03:51:24 -0500

    Restructure Scripts Folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import mainGame from "./Game/MainGame.js";
 
-import test from "./Game/Scripts/test.js";
+import test from "./Scripts/test.js";
 async function main() {
     const canvas = document.getElementById("canvas");
     canvas.style.border = "1px solid red";
